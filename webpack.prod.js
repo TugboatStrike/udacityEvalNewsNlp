acd78952181ffd5e2caf87b7aa1ec64bce0f8f40
Byron Meals <bmeals87@gmail.com>
2022-05-25 17:57:29 -0600

    Got mini-css-extract-plugin working on production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    /*optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
+    },*/
     output: { // creates a library for the global to call local functions.
         libraryTarget: 'var',
         library: 'Client'
@@ -17,8 +23,13 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                //test: /\.scss$/,
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /node_modules/,
+                //use: ['style-loader', 'css-loader', 'sass-loader']
+                // in order to use this MiniCssExtractPlugin i had to bump
+                // webpack version to 4.40 ...
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             }
         ]
     },
@@ -26,6 +37,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
         })
     ]
 }
