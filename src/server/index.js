11b010f6f08bf326dbee6c26f428fbdf2759fd1a
Byron Meals <bmeals87@gmail.com>
2022-06-01 12:31:32 -0600

    Added meaning cloud sentiment api call to server

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require('dotenv');
 
+//const FormData = require('form-data'); // required for meaning cloud js call
+const fetch = require('node-fetch'); // required for fetch
+
 
 dotenv.config(); // allowing the .env file to be called by dotenv module.
 
@@ -27,5 +30,45 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
+// meaning cloud api call details
+mc = {
+  https: 'https://',
+  hostname:  "api.meaningcloud.com",
+  key: process.env.MEANING_CLOUD_API_KEY,
+  lang: "en",
+  types: {
+    sent: '/sentiment-2.1?key=',
+  },
+  fetchOp: {
+    method: 'POST',
+    redirect: 'follow'
+  },
+  /*
+  url: function() {
+    return String.raw`${ht}${hn}${type}${key}&lang=${lg}&txt=${tx}&model=${mod}`
+  }*/
+};
 
-//console.log(`Api Key: ${process.env.MEANING_CLOUD_API_KEY}`);
+// create url for meaning cloud api url fetch call
+function mcUrl(text, mcType) {
+  const tx = encodeURIComponent(text);
+  const ht = mc.https;
+  const hn = mc.hostname;
+  const key = mc.key;
+  const lg = mc.lang;
+  const type = mcType;
+  const mod = 'test';
+  const url = String.raw`${ht}${hn}${type}${key}&lang=${lg}&txt=${tx}`;
+  //const url = String.raw`${ht}${hn}${type}${key}&lang=${lg}&txt=${tx}&model=${mod}`;
+  return url;
+};
+
+// meaning cloud sentiment api fetch
+async function sentFetch(text) {
+  const response = await fetch(mcUrl(text, mc.types.sent),mc.fetchOp)
+  const jsonInfo = await response.json();
+  console.log('jsonInfo: ', jsonInfo);
+  return response;
+};
+
+sentFetch(`test text`);
